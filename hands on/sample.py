322055e3d375c0f5051cffeabacc823aaf7ed2b4
krim798 <168003081+Krishan098@users.noreply.github.com>
2025-05-09 22:12:28 +0530

    start of svm

---
+++ hands on/sample.py
@@ -0,0 +1,8 @@
+import scipy
+from scipy.stats import fisher_exact
+import numpy as np
+import scipy.stats
+# table=np.array([[39,63],[30961,30937]])
+# print(fisher_exact(table,'less'))
+
+print(scipy.stats.norm.cdf(-3.02679296047))
